b5eb178d3fb90b7363c22681aa5137e578185353
edmarfjr <edmarjr@outlook.com>
2025-01-07 09:29:09 -0300

    mudança de cores para preto e branco

--- styles/index.ts
+++ styles/index.ts
@@ -5,35 +5,38 @@ const styles = StyleSheet.create({
     container:{
         flex: 1,
        alignItems: "center",
-       backgroundColor: '#f5f5f5',
+       backgroundColor: 'black',
        padding: 20
       },
       title:{
         fontWeight:'bold',
         fontSize:32,
-        color: 'green',
+        color: 'white',
         marginBottom: 10
       },
       input:{
         width: '100%',
         height:50,
-        backgroundColor: '#FFF',
+        backgroundColor: 'black',
         borderRadius: 10,
         paddingHorizontal: 10,
         borderWidth: 1,
-        borderColor: "#ddd",
-        marginBottom:20
+        borderColor: "white",
+        marginBottom:20,
+        color: 'white'
       },
       button:{
-        backgroundColor:"#4ecdc4",
+        backgroundColor:"white",
         alignItems:"center",
         borderRadius:10,
         justifyContent:'center',
-        width:'100%'
+        width:'100%',
+        height:50
       },
       buttonText:{
         fontWeight:'bold',
-        color:"#fff"
+        color:"black",
+        justifyContent:'center'
       },
       card:{
         marginTop:30,
@@ -41,18 +44,18 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding:20,
         borderWidth: 1,
-        backgroundColor: '#FFF',
-        borderColor: "#ddd"
+        backgroundColor: 'black',
+        borderColor: "white"
       },
       cardTitle:{
         fontSize:16,
         fontWeight:'bold',
-        color: '#333',
+        color: 'white',
         marginBottom:10,
       },
       cardText:{
         fontSize:16,
-        color: '#666'
+        color: 'white'
       },
       image: {
         width: 200,
